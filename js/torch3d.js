b5c156e3c0427e3c7c33747f1fd27b0af7721ae0
Matthew Gordon <inverarity@gmail.com>
2016-11-13 13:08:40 -0800

    Added a sphere to represent a flame

--- js/torch3d.js
+++ js/torch3d.js
@@ -2,7 +2,7 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-camera.position.set(0, 10, 100);
+camera.position.set(0, 10, 50);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 var scene = new THREE.Scene();
 
@@ -20,16 +20,19 @@ var scale = 10;
 // 			[1,1,1]]
 var h = Math.sqrt(3)/2
 var c = 1/3
-vertices = [
-	[0, 0, 0], [1, 0, 0], [0.5, 0, h],
-	[0, 0, 0], [0.5, h, c], [1, 0, 0],
-	[0, 0, 0], [0.5, 0, h], [0.5, h, c],
+var vertices = {v0: [0.5, h, c], v1: [0, 0, 0], v2: [1, 0, 0], v3: [0.5, 0, h], v4: [0.5, -h, c]} ;
 
-	[0, 0, 0], [0.5, -h, c], [1, 0, 0],
-	[0, 0, 0], [0.5, 0, h], [0.5, -h, c],
-	[0, 0, 0]
+var draw_vertices = [
+    vertices.v1, vertices.v2, vertices.v3, // X-Z plane triangle
+    vertices.v1, vertices.v0, vertices.v2, // top tetrahedron side 1
+    vertices.v3, vertices.v0, // top tetrahedron side 2
+
+    vertices.v1, vertices.v4, vertices.v2, // bottom tetrahedron side 2
+    vertices.v4, vertices.v3 // bottom tetrahedron side 2
+
+
 ]
-vertices.forEach(function(v) { 
+draw_vertices.forEach(function(v) { 
 	geometry.vertices.push(new THREE.Vector3(v[0]*scale, v[1]*scale, v[2]*scale));
 })
 // geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
@@ -37,9 +40,13 @@ vertices.forEach(function(v) {
 // geometry.vertices.push(new THREE.Vector3(10, 0, 0));
 
 var line = new THREE.Line(geometry, material);
-// line.position.x -= 0.5*scale;
-// line.position.z += c*scale/2; 
+var sphere_geo = new THREE.SphereGeometry( 1, 32, 32 );
+var sphere_material = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+var sphere = new THREE.Mesh( sphere_geo, sphere_material );
+//sphere.position.set(0, 0, 0);
+
 scene.add(line);
+scene.add(sphere);
 //renderer.render(scene, camera);
 var rotation_speed = 0.02;
 document.setRotation = function() {
