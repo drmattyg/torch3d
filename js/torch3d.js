40ae8100b61d9c447326fe2c2b399564e7c14fde
Matthew Gordon <inverarity@gmail.com>
2017-01-01 21:20:48 -0800

    Cleanupo

--- js/torch3d.js
+++ js/torch3d.js
@@ -9,13 +9,10 @@ var jsyaml = require('js-yaml');
 var fs = require('fs');
 var editorControl = require('./editor.js');
 var AudioPlayer = require('./audio.js')
-//document.body.appendChild( container );
 
 renderer.setSize(300, 600);
-//document.body.appendChild(renderer.domElement);
 container.appendChild( renderer.domElement );
 var camera = new THREE.PerspectiveCamera(45, 0.5, 1, 500);
-// camera.position.set(5, 20, 50);
 camera.position.set(5, 20, 50);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 
@@ -46,8 +43,6 @@ function updateTimer(time) {
     $("#play-timer-time").text(Math.floor(time));
 }
 
-// fix mp3 hosting and failure
-// why doesn't chaser run?
 
 function resetAndRun(sb) {
     resetSettings();
@@ -67,11 +62,9 @@ function runEditorSongbook() {
     var text = editor.getValue();
     try {
         var yml = jsyaml.safeLoad(text);
-//            torchModel = new TorchModel(scale, scene);
         new Songbook(text, torchModel, (sb) => { 
             window.current_songbook = sb;
             audioPlayer.init(sb);
-            console.trace();
             if(audioPlayer.mediaUrl) {
                 audioPlayer.play(() => {
                     resetAndRun(sb);
@@ -94,7 +87,6 @@ function resetSettings() {
     $("#run-button").html('<i class="fa fa-play"></i>');
     $("#pause-button").html('<i class="fa fa-pause"></i>');
     updateTimer(0);
-    //audioPlayer.localAudio = false;
 }
 
 var examples = ['song_for_diana', 'chaser'];
@@ -152,13 +144,3 @@ $(document).ready(function(){
         }
     });
 });
-// var xhttp = new XMLHttpRequest();
-// xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//         var sb = new Songbook(this.responseText, torchModel);
-//         sb.run();
-//     }
-// };
-// xhttp.open("GET", "songbooks/test.yaml", true);
-// xhttp.send();
-
